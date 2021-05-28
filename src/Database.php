e27332a46a6bcf773a387b3b9e48148ad4ea660c
mxmilyasov <mxmilyasov@gmail.com>
2021-05-28 23:29:11 +0300

    Test commit db env.

--- src/Database.php
+++ src/Database.php
@@ -9,8 +9,8 @@ class Database
 
     public static function getConnection(): PDO
     {
-        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-        $dotenv->load();
+//        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+//        $dotenv->load();
 
         $host = $_ENV['DB_HOST'];
         $dbname = $_ENV['DB_NAME'];
